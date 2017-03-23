26f66aabfe61342b88b7804304acabaf006720de
Michelle kassab <michelle.kassab@mdycehs.net>
2017-03-23 14:54:33 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+    createCanvas(900,900);
+}
+function draw() {
+  stroke(66, 134, 244);
+  strokeWeight(10);
+  fill(255, 255,255,1);
+  ellipse(150, 150, 160 , 160);  
+  
+  stroke(0);
+  strokeWeight(10);
+  fill(155,155,155,1);
+  ellipse(330,150,160,160);
+  
+  stroke(244, 66, 66);
+  strokeWeight(10);
+  fill(155,155,155,1);
+  ellipse(510,150,160,160); 
+  
+  stroke(244, 232, 65);
+  strokeWeight(10);
+  fill(155,155,155,1);
+  ellipse(240,230,160,160);
+  
+  stroke(121, 244, 65);
+  strokeWeight(10);
+  fill(155,155,155,1);
+  ellipse(415,230,160,160);
+}
